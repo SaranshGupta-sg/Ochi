fe300cfa2072514ca141507070a866d76e29e661
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-17 22:28:11 +0530

    new

--- README.md
+++ README.md
@@ -30,3 +30,12 @@ cards
 cards
 ready to start 
 footer
+
+
+
+
+
+
+
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Navbar />
-  )
-}
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <Navbar />
+    </div>
+  );
+};
 
-export default App
+export default App;

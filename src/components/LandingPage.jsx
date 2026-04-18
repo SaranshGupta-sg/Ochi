513ff886a7de90684d449f2867f3a67e1e517582
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-18 23:12:37 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,17 +5,17 @@ const LandingPage = () => {
     <div className="w-full h-screen bg-zinc-900 pt-1">
       <div className="textstructure mt-40 px-20">
         <div className="masker">
-          <h1 className="text-9xl leading-none tracking-tighter font-medium title2">
+          <h1 className="text-8xl leading-[6.5vw] tracking-tighter font-medium title2">
             WE CREATE
           </h1>
         </div>
         <div className="masker">
-          <h1 className="text-9xl leading-none tracking-tighter font-medium title2">
+          <h1 className="text-8xl leading-[6.5vw] tracking-tighter font-medium title2">
             EYE OPENING
           </h1>
         </div>
         <div className="masker ">
-          <h1 className="text-9xl leading-none tracking-tighter font-medium title2">
+          <h1 className="text-8xl leading-[6.5vw] tracking-tighter font-medium title2">
             PRESENTATIONS
           </h1>
         </div>

e03bf6d2280700cb4f1b097c7e9441557b60f9a3
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-18 22:27:14 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const LandingPage = () => {
   return (
-    <div>
-      LandingPage
+    <div className="w-full h-screen bg-zinc-900 pt-1">
+      <div className="textstructure mt-40 px-20">
+        <div className="masker ">
+          <h1 className="text-9xl title2">WE CREATE</h1>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;

8708a87b328b8f220098f75380c59cf45206934c
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-19 22:09:08 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,14 +4,15 @@ const LandingPage = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 pt-1">
       <div className="textstructure mt-40 px-20">
-        {["We Create", "Eye Opening", "Presentations"].map((item,index)=>{
-          
+        {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
+          return (
+            <div className="masker">
+              <h1 className="text-8xl leading-[6.5vw] tracking-tighter uppercase font-medium title2">
+                {item}
+              </h1>
+            </div>
+          );
         })}
-        <div className="masker ">
-          <h1 className="text-8xl leading-[6.5vw] tracking-tighter font-medium title2">
-            PRESENTATIONS
-          </h1>
-        </div>
       </div>
     </div>
   );

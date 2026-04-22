afa5918497927ea9bcec9fa1cf211b066d2f040e
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-22 22:05:17 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -26,7 +26,7 @@ const LandingPage = () => {
           </p>
         ))}
         <div className="start flex items-center gap-5">
-          <div className="px-5 py-2 border-[2px] border-zinc-500 font-light text-sm capitalize rounded-full">
+          <div className="px-5 py-2 border-[2px] border-zinc-500 font-light text-md uppercase capitalize rounded-full">
             Start the project
           </div>
           <div className="w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full">

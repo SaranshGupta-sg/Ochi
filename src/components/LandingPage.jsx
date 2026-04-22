8e7a29408cc5aaafbfcf55438da71d96af39a1ce
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-22 22:03:56 +0530

    new

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -29,8 +29,8 @@ const LandingPage = () => {
           <div className="px-5 py-2 border-[2px] border-zinc-500 font-light text-sm capitalize rounded-full">
             Start the project
           </div>
-          <div className="w-10 h-10 flex items-center justify-center border-[2px] border-zinc-500 rounded-full">
-            <span>
+          <div className="w-10 h-10 flex items-center justify-center border-[1px] border-zinc-400 rounded-full">
+            <span className="rotate-[45deg]">
               <FaArrowUpLong />
             </span>
           </div>

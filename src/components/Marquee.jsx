3f2a4d9f16519135d49c67fae2720e04a89e1886
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-23 23:49:36 +0530

    new

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Marquee = () => {
   return (
-    <div className='w-full h-[50vh] bg-[#004D43]'>
-      
+    <div className="w-full py-10 bg-[#004D43]">
+      <div className="text border-t-2 border-b-2 border-zinc-300">
+        <h1>we are ochi</h1>
+        <h1>we are ochi</h1>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Marquee
+export default Marquee;

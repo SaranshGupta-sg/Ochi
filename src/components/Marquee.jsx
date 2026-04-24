143d10cb3739e70596593cf616e5a2539e5112df
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-24 17:46:10 +0530

    new

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Marquee = () => {
   return (
     <div className="w-full py-10 bg-[#004D43]">
-      <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap">
-        <h1 className="text-[10vw] leading-none title uppercase">we are ochi</h1>
-        <h1 className="text-[10vw] leading-none title uppercase">we are ochi</h1>
+      <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap">
+        <h1 className="text-[15vw] leading-none title uppercase ">we are ochi</h1>
+        <h1 className="text-[15vw] leading-none title uppercase ">we are ochi</h1>
       </div>
     </div>
   );

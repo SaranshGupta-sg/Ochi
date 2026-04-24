31e5d77b87e39920bfb6eb252c6e824480bb9acd
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-24 17:43:30 +0530

    new

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -4,8 +4,8 @@ const Marquee = () => {
   return (
     <div className="w-full py-10 bg-[#004D43]">
       <div className="text border-t-2 border-b-2 border-zinc-300 flex whitespace-nowrap">
-        <h1 className="text-[10vw] leading-none title">we are ochi</h1>
-        <h1 className="text-[10vw] leading-none title">we are ochi</h1>
+        <h1 className="text-[10vw] leading-none title uppercase">we are ochi</h1>
+        <h1 className="text-[10vw] leading-none title uppercase">we are ochi</h1>
       </div>
     </div>
   );

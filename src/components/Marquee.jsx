90abf6e88fc8d2e4a2362be57c06b78de2da2a5e
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-24 17:56:39 +0530

    new

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Marquee = () => {
   return (
     <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
       <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap">
-        <h1 className="text-[18vw] leading-none title uppercase ">we are ochi</h1>
-        <h1 className="text-[18vw] leading-none title uppercase ">we are ochi</h1>
+        <motion.h1 className="text-[18vw] leading-none title uppercase ">
+          we are ochi
+        </motion.h1>
+        <motion.h1 className="text-[18vw] leading-none title uppercase ">
+          we are ochi
+        </motion.h1>
       </div>
     </div>
   );

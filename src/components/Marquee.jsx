b1a3b042d866feb98712338cda3fca9bfaca1325
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-25 17:32:00 +0530

    new

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -5,10 +5,28 @@ const Marquee = () => {
   return (
     <div className="w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]">
       <div className="text border-t-2 border-b-2 border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap">
-        <motion.h1 className="text-[18vw] leading-none title uppercase ">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[22vw] leading-none title uppercase "
+        >
           we are ochi
         </motion.h1>
-        <motion.h1 className="text-[18vw] leading-none title uppercase ">
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[22vw] leading-none title uppercase "
+        >
+          we are ochi
+        </motion.h1>
+        <motion.h1
+          initial={{ x: 0 }}
+          animate={{ x: "-100%" }}
+          transition={{ ease: "linear", repeat: Infinity, duration: 5 }}
+          className="text-[22vw] leading-none title uppercase "
+        >
           we are ochi
         </motion.h1>
       </div>

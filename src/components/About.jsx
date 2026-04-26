14041f9feccb5da5e4739c3229758fc13c71cdf0
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-26 20:59:25 +0530

    new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="w-full p-20 bg-[#CDEA68] rounded-tl-3xl rounded-tr-3xl text-black">
-      <h1 className="text-[3vw] leading-[3.5vw] tracking-tight title2">
+      <h1 className="text-[3vw] leading-[4.5vw] tracking-tight title2">
         We craft category-defining presentations, brand identities, and digital
         experiences that drive funding, sales, and market leadership.
       </h1>

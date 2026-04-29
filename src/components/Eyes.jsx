7a9895fd9e2b8a6186cd85a3ad2723428aa7c8c4
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-04-29 19:12:20 +0530

    new

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Eyes = () => {
   return (
     <div className='eyes h-screen overflow-hidden'>
-      <div className='w-full h-full bg-red-500'></div>
+      <div className='w-full h-full bg-red-500'>
+        <h1>Helllooo</h1>
+      </div>
     </div>
   )
 }

6826d5ba68594a1ce4af98f578398ba5e1ae2c9e
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-04 20:15:43 +0530

    new

--- src/components/Hey.jsx
+++ src/components/Hey.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Hey = () => {
   return (
-    <div>
-      
+    <div className="h-screen overflow-hidden">
+      <div className="w-full h-full bg-red-500">
+        <h1 className="bg-green-300">konbanwa</h1>
+        <div>
+          <h1 className="bg-pink-300">ohayo</h1>
+        </div>
+        <div>
+          <h1 className="bg-orange-400">GSAPP</h1>
+          <h2 className="bg-amber-300">samping</h2>
+          <h3>Going</h3>
+        </div>
+        <div>
+          <h1 className="bg-orange-400">GSAPP</h1>
+          <h2 className="bg-amber-300">samping</h2>
+          <h3>Going</h3>
+        </div>
+        <div>
+          <h1>Git</h1>
+          <h1 className="bg-pink-100">Github</h1>
+        </div>
+        <div className="text-5xl bg-amber-50">
+          <h1 className="h-2">HEy man</h1>
+          <h3>keyword</h3>
+        </div>
+        <div className="text-5xl bg-amber-50">
+          <h1 className="h-2">HEy man</h1>
+          <h3>keyword</h3>
+        </div>
+        <div className="text-5xl bg-amber-50">
+          <h1 className="h-2">HEy man</h1>
+          <h3>keyword</h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hey
+export default Hey;

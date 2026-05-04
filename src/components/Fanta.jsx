50c4b5bbc82d4661dc3abf0d513609f2223f5a74
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-04 20:18:06 +0530

    new

---
+++ src/components/Fanta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Fanta = () => {
+  return (
+    <div>
+      <h1 className='bg-pink-400 text-3xl'>Omae no otokono no nano ka</h1>
+    </div>
+  )
+}
+
+export default Fanta

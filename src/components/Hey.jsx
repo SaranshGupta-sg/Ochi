0ceefda1251c58b2dbdd003e5ab0c65e52acd303
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-05 21:53:42 +0530

    new

--- src/components/Hey.jsx
+++ src/components/Hey.jsx
@@ -34,6 +34,10 @@ const Hey = () => {
           <h1 className="h-2">HEy man</h1>
           <h3>keyword</h3>
         </div>
+        <div className="text-5xl bg-amber-50">
+          <h1 className="h-2">HEy man</h1>
+          <h3>keyword</h3>
+        </div>
       </div>
     </div>
   );

727a6f4e314ad5bc42b20233c09799ed30f782d1
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-12 22:41:29 +0530

    new

--- src/components/Eyes.jsx
+++ src/components/Eyes.jsx
@@ -4,17 +4,24 @@ const Eyes = () => {
   const [rotate, setRotate] = useState(0);
 
   useEffect(() => {
-    window.addEventListener("mousemove", (e) => {
+    const handleMouseMove = (e) => {
       let mouseX = e.clientX;
       let mouseY = e.clientY;
 
-      let delayX = mouseX - window.innerWidth / 2;
-      let delayY = mouseY - window.innerHeight / 2;
+      let deltaX = mouseX - window.innerWidth / 2;
+      let deltaY = mouseY - window.innerHeight / 2;
 
-      var angle = Math.atan2(y, x) * (180 / Math.PI);
-      setRotate(angle);
-    });
-  });
+      let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+
+      setRotate(angle - 180);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
   return (
     <div className="eyes h-screen overflow-hidden">
@@ -26,34 +33,36 @@ const Eyes = () => {
           backgroundPosition: "center",
         }}
       >
-        <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] ">
-          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
-            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
-              <div
-                style={{
-                  transform: `translate(-50% -50%) rotate(${rotate}deg)`,
-                }}
-                className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10"
-              >
-                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
-              </div>
-            </div>
-          </div>
-
-          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
-            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
-              <div
-                style={{
-                  transform: `translate(-50% -50%) rotate(${rotate}deg)`,
-                }}
-                className="line absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] w-full h-10"
-              >
-                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
-              </div>
-            </div>
-          </div>
-
+        <div className="absolute flex gap-10 top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]">
           
+          {/* Left Eye */}
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                }}
+                className="absolute top-1/2 left-1/2 w-full h-10"
+              >
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+
+          {/* Right Eye */}
+          <div className="flex items-center justify-center w-[15vw] h-[15vw] rounded-full bg-zinc-100">
+            <div className="relative w-2/3 h-2/3 rounded-full bg-zinc-900">
+              <div
+                style={{
+                  transform: `translate(-50%, -50%) rotate(${rotate}deg)`,
+                }}
+                className="absolute top-1/2 left-1/2 w-full h-10"
+              >
+                <div className="w-10 h-10 rounded-full bg-zinc-100"></div>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>

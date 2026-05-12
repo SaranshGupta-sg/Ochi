af2c8654eb3c58c4c887d6b1b878c9b432d1b4e5
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-05-13 00:18:59 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Eyes />
       <Featured />
       <Cards />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900'>
+      <h1 className='text-8xl text-center mt-50'>Hello Footer</h1>
+    </div>
+  )
+}
+
+export default Footer
